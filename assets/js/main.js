f8979fed1adaa940c4cc0f6ac03ef17d7787563e
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-01-08 15:29:51 +0100

    Creato un ciclo for per creare mail ed inserirle nell'array per 10 volte
    Creato un ciclo for per creare mail ed inserirle nell'array per 10 volte, poi nell' html le stampo con un ciclo v-for

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,23 @@
 let app = new Vue({
     el: "#app",
     data:{
+        listaEmail: [],
 
     },
     mounted(){
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(response =>{
-            //verifico lo stato della risposta
-            //console.log(response);
-            //stampo su console un email random
-            console.log(response.data.response);
-        })
+        //creo un ciclo for che per 10 volte creera una mail e la inserira all'interno dell'array listaEmail
+        for (let index = 0; index < 10; index++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(response =>{
+                //verifico lo stato della risposta
+                //console.log(response);
+                //stampo su console un email random
+                //console.log(response.data.response);
+                //creo una variabile in cui salvare l'e-mail
+                const email = response.data.response;
+                //inserisco l'email all'interno dell'array listaEmail
+                this.listaEmail.push(email);
+            }) 
+        }
     }
 });
